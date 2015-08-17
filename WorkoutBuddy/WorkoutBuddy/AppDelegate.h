b8433e6162f723333cd4900a670ad630a31a269f
Oscar G.M <oscar@wustl.edu>
2015-08-16 21:17:25 -0500

    added project

---
+++ WorkoutBuddy/WorkoutBuddy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WorkoutBuddy
+//
+//  Created by Oscar G.M on 8/16/15.
+//  Copyright (c) 2015 GMStudios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WorkoutBuddy/WorkoutBuddy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WorkoutBuddy
+//
+//  Created by Oscar G.M on 8/16/15.
+//  Copyright (c) 2015 GMStudios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
