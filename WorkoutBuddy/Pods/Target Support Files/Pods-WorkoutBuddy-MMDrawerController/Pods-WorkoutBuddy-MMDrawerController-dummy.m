fe5dd01344f92395516413cdc16ee43b3bf9a42a
Oscar G.M <oscar@wustl.edu>
2015-08-16 23:06:47 -0500

    Added sidebar library

---
+++ WorkoutBuddy/Pods/Target Support Files/Pods-WorkoutBuddy-MMDrawerController/Pods-WorkoutBuddy-MMDrawerController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WorkoutBuddy_MMDrawerController : NSObject
+@end
+@implementation PodsDummy_Pods_WorkoutBuddy_MMDrawerController
+@end
--- WorkoutBuddy/WorkoutBuddy/AppDelegate.m
+++ WorkoutBuddy/WorkoutBuddy/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "MemberViewViewController.h"
 
 @interface AppDelegate ()
 
