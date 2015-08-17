13fd5fa5167188d90973e51ccfe3eec3ef0174ed
Oscar G.M <oscar@wustl.edu>
2015-08-17 00:59:47 -0500

    Added some basic classes includin a table view controller

---
+++ WorkoutBuddy/WorkoutBuddy/MemberViewTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MemberViewTableViewController.h
+//  WorkoutBuddy
+//
+//  Created by Oscar G.M on 8/16/15.
+//  Copyright (c) 2015 GMStudios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MemberViewTableViewController : UITableViewController
+
+@end
