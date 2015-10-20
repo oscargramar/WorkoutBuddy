31f004acef1070d1304a4f776d35656b1efda4f9
Oscar G.M <oscar@wustl.edu>
2015-10-19 22:32:38 -0500

    updated xcode settings

---
+++ dariusLab/dariusLab/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dariusLab
+//
+//  Created by Oscar G.M on 9/17/15.
+//  Copyright (c) 2015 GMStudios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
