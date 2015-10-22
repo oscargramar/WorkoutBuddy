d289bc6ea113e638b21375f278f4e55d0bd8b2d4
Oscar G.M <oscar@wustl.edu>
2015-10-21 19:52:26 -0500

    attempting to retrieve members

---
+++ WorkoutBuddy/WorkoutBuddy/Member.h
@@ -0,0 +1,13 @@
+//
+//  Member.h
+//  WorkoutBuddy
+//
+//  Created by Oscar G.M on 10/21/15.
+//  Copyright © 2015 GMStudios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Member : NSObject
+
+@end
---
+++ WorkoutBuddy/WorkoutBuddy/Member.m
@@ -0,0 +1,13 @@
+//
+//  Member.m
+//  WorkoutBuddy
+//
+//  Created by Oscar G.M on 10/21/15.
+//  Copyright © 2015 GMStudios. All rights reserved.
+//
+
+#import "Member.h"
+
+@implementation Member
+
+@end
